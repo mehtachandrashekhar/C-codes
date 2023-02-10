7672d9d94dc997dd06da483898fdf6fc1c3cc334
Chandrashekhar <67406458+mehtachandrashekhar@users.noreply.github.com>
2023-02-10 19:08:50 +0530

    Merge_sort

--- DSA/quick_sort.c
+++ DSA/quick_sort.c
@@ -1,22 +1,59 @@
-//quick sort
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
+void quicksort (int [], int, int);
+int main()
+{
+    int list[50];
+    int size, i;
 
-void  quick_sort(int[10],int,int);
-int partiotion(int[10],int,int);
-
-int main(){
-     int x[20],n,i;
-
-     printf("\nEnter the size of array :");
-     scanf("%d",&n);
-
-     for(i=0;i<n;i++)
-     {
-        scanf("%d",&x[i]);
-     }
-
-     
-
+    printf("Enter the number of elements: ");
+    scanf("%d", &size);
+    
+    printf("Enter the elements to be sorted:\n");
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &list[i]);
+    }
+    quicksort(list, 0, size - 1);
+    printf("After applying quick sort\n");
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", list[i]);
+    }
+    // printf("\n");
     return 0;
+}
+
+void quicksort(int list[], int low, int high)
+{
+    int pivot, i, j, temp;
+    if (low < high)
+    {
+        pivot = low;
+        i = low;
+        j = high;
+        while (i < j)
+        {
+            while (list[i] <= list[pivot] && i <= high)
+            {
+                i++;
+            }
+            while (list[j] > list[pivot] && j >= low)
+            {
+                j--;
+            }
+            if (i < j)
+            {
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+            }
+        }
+            temp = list[j];
+            list[j] = list[pivot];
+            list[pivot] = temp;
+
+        quicksort(list, low, j - 1);
+        quicksort(list, j + 1, high);
+    }
 }
