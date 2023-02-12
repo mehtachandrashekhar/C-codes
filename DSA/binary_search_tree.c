68a1bc97c17da94bfa3c6347f364986dbd7e73ab
Chandrashekhar <67406458+mehtachandrashekhar@users.noreply.github.com>
2023-02-12 08:23:30 +0530

    update_binary_Search_Tree

--- DSA/binary_search_tree.c
+++ DSA/binary_search_tree.c
@@ -1,101 +1,124 @@
-// creating a binary serarch tee
 #include<stdio.h>
-#include<stdlib.h>
+#include<malloc.h>
 
-struct node *create_node(int data);
-void insert_data(int data);
-void inorder(struct node *node);
-
-struct node {
+struct node{
     int data;
-    struct node *right, *left;
+    struct node* left;
+    struct node* right;
 };
 
-struct node *root = NULL;
-
-int main()
-{
-    // int choice;
-    // printf("enter choice:");
-    // scanf("%d,&choice");
-
-    *create_node(5);
-    *create_node(78);
-    *create_node(23);
-    // switch(choice)
-    // {
-    //     case 1: *create_node(); break;
-    //     case 2: insert_data();  break;
-    //     case 3: inorder();      break;
-    //     default: printf("Invalid input");
-    // }
-    return 0;
-}
-struct node *create_node(int data)
-{
-    struct node *newnode;
-    newnode = (struct node*)malloc(sizeof(struct node));
-    newnode->data=data;
-    newnode->left=NULL;
-    newnode->right=NULL;
-    return newnode;
-
+struct node* createNode(int data){
+    struct node *n; // creating a node pointer
+    n = (struct node *) malloc(sizeof(struct node)); // Allocating memory in the heap
+    n->data = data; // Setting the data
+    n->left = NULL; // Setting the left and right children to NULL
+    n->right = NULL; // Setting the left and right children to NULL
+    return n; // Finally returning the created node
 }
 
-void insert_data(int data)
-{
-    struct node *newnode = create_node(data);
-    if(root==NULL)
-    {
-        root=newnode;
-        return;
+void preOrder(struct  node* root){
+    if(root!=NULL){
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
     }
-    else
-    {
-        struct node *current = root;
-        struct node *parent = NULL;
-        while (1)
-        {
-            parent=current;
-            if(data < current->data)
-            {
-                current=current->left;
-            }
-            else if (data>current->data) 
-            {
-              current=current->right;
-              if(current==NULL)
-              {
-                parent->right = newnode;
-              } 
-            }   
+}
 
-            else if (current=NULL)
-            {
-                parent->left=newnode;
-            }
-            return;
+void postOrder(struct  node* root){
+    if(root!=NULL){
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+void inOrder(struct  node* root){
+    if(root!=NULL){
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+}
+
+int isBST(struct  node* root){
+    static struct node *prev = NULL;
+    if(root!=NULL){
+        if(!isBST(root->left)){
+            return 0;
+        }
+        if(prev!=NULL && root->data <= prev->data){
+            return 0;
+        }
+        prev = root;
+        return isBST(root->right);
+    }
+    else{
+        return 1;
+    }
+}
+
+struct node * searchIter(struct node* root, int key){
+    while(root!=NULL){
+        if(key == root->data){
+            return root;
+        }
+        else if(key<root->data){
+            root = root->left;
+        }
+        else{
+            root = root->right;
         }
     }
+    return NULL;
 }
 
-void inorder(struct node *node)
-{
-if (root == NULL)
-{
-    printf("tree is empty");
-    return;
+void insert(struct node *root, int key){
+   struct node *prev = NULL;
+   while(root!=NULL){
+       prev = root;
+       if(key==root->data){
+           printf("Cannot insert %d, already in BST", key);
+           return;
+       }
+       else if(key<root->data){
+           root = root->left;
+       }
+       else{
+           root = root->right;
+       }
+   }
+   struct node* new = createNode(key);
+   if(key<prev->data){
+       prev->left = new;
+   }
+   else{
+       prev->right = new;
+   }
+
 }
-else
-{
-    if(node->left!=NULL)
-    {
-        inorder(node->left);
-        printf("%d",node->data);
-    }
-    if (node->right=NULL)
-    {
-        inorder(node->right);
-    }
-}
+
+int main(){
+     
+    // Constructing the root node - Using Function (Recommended)
+    struct node *p = createNode(5);
+    struct node *p1 = createNode(3);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(1);
+    struct node *p4 = createNode(4);
+    // Finally The tree looks like this:
+    //      5
+    //     / \
+    //    3   6
+    //   / \
+    //  1   4  
+
+    // Linking the root node with left and right children
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    insert(p, 16);
+    printf("%d", p->right->right->data);
+    return 0;
 }
