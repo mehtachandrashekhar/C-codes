13f3d383a8c81c0c4ce4d085171b3150f1008299
mehtachandrashekhar <67406458+mehtachandrashekhar@users.noreply.github.com>
2023-02-21 20:42:27 +0530

    convert_infix_to_Postfix_completed

--- DSA/convert_infix_to_Postfix.c
+++ DSA/convert_infix_to_Postfix.c
@@ -3,23 +3,24 @@
 #include<string.h>
 #define MAX 100
 
-char infix[MAX],postfix[100];
-char stack[MAX]
-int top -1;
+char infix[MAX],postfix[MAX];
+char stack[MAX];
+int top = -1;
 
-void inToPostfix();
+void push(char);
 char pop();
-char push();
-int precedence(char);
+int isEmpty();
+void inToPostfix();
 void print();
-void isEmpty();
+int precedence(char);
+
 int main()
 {
     printf("Enter the infix expression:");
     gets(infix);
 
     inToPostfix();
-    void print();
+    print();
 }
 
 void inToPostfix()
@@ -46,27 +47,77 @@ void inToPostfix()
             case '-' :
             case '*' :
             case '/' :
-                while(!isEmpty[top] && precedence(stack[top]) >= precedence(symbol))
+                while(!isEmpty() && precedence(stack[top]) >= precedence(symbol))
                 {
-                    postfix[j] = pop();
+                    postfix[j++] = pop();
                 }
                 push(symbol);
                 break;
+            default : // if symbol is a operand
+            postfix[j++] = symbol;                
 
         }
     }
     
-    while (!isEmpty())
+    while(!isEmpty())
     {
         postfix[j++] = pop();
     }
     postfix[j] = '\0';
 }
 
-void precedence(char)
+int precedence(char symbol)
 {
-    switch(char)
+    switch(symbol)
     {
-        
+      case '^' : return 3;
+      case '/' : 
+      case '*' : return 2;
+      case '+' : 
+      case '-' : return 1;
+      default  : return 0;
+
     }
 }
+ 
+ void print()
+ {
+    int i=0;
+    printf("\nThe postfix expression is :");
+    while(postfix[i])
+    {
+        printf("%c",postfix[i++]);
+    }
+ }
+
+ void push(char c)
+ {
+    if(top == MAX-1)
+    {
+        printf("stack overflow");
+        return;
+    }
+    top++;
+    stack[top] = c;
+ }
+
+ char pop()
+ {
+    char c;
+    if(top == -1)
+    {
+        printf("stack underflow");
+        exit(1);
+    }
+    c  = stack[top];
+    top = top-1;
+    return c;
+ }
+
+ int isEmpty()
+ {
+    if(top == -1)
+        return 1;
+    else
+        return 0;
+ }
